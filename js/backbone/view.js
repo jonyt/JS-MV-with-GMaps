44047e7a87fdfd1608354edd9f3de33cc0e42733
Eric Bréchemier <eric@bezen.org>
2013-09-14 10:22:29 +0200

    Remove trailing spaces from end of lines

--- js/backbone/view.js
+++ js/backbone/view.js
@@ -2,13 +2,13 @@ AppView = Backbone.View.extend({
       el: $("body"),
       map : null,
       template : null,
-      initialize: function () {        
+      initialize: function () {
         this.places = new Places( null, { view: this }); // Initialize the collection, we have no initial value so we pass in null, but we do want to set the view
         map = new google.maps.Map(document.getElementById('map'), { // Create the map
           mapTypeId: google.maps.MapTypeId.ROADMAP,
           center: new google.maps.LatLng(-33.8665433,151.1956316),
           zoom: 15
-        });    
+        });
         map.markers = [];
         template = _.template("<li><b>Name</b>: <%= attributes.name %>, <b>Rating</b>: <%= attributes.rating %>, <b>Near</b>: <%= attributes.vicinity %>  <img class='place-image' src='<%= attributes.icon %>'></li>");
       },
@@ -18,9 +18,9 @@ AppView = Backbone.View.extend({
       getPlaces: function () {
         console.log('hi');
         $('#places').empty(); // Delete the places list
-        $(map.markers).each(function (index, marker) { marker.setMap(null); }); // Delete all map markers        
+        $(map.markers).each(function (index, marker) { marker.setMap(null); }); // Delete all map markers
         map.markers = [];
-        
+
         var request = {
             location: map.getCenter(),
             radius: '500',
@@ -29,8 +29,8 @@ AppView = Backbone.View.extend({
         var theView = this;
         var service = new google.maps.places.PlacesService(map);
         service.search(request, function callback(results, status) {
-          if (status == google.maps.places.PlacesServiceStatus.OK) {                                                
-            results = results.slice(0, 10); // We want only the first 10 results          
+          if (status == google.maps.places.PlacesServiceStatus.OK) {
+            results = results.slice(0, 10); // We want only the first 10 results
             $.each(results, function(index, place){
                 var placeModel = new Place({ // Create a model for this result
                     name       : place.name,
@@ -39,21 +39,21 @@ AppView = Backbone.View.extend({
                     icon       : place.icon,
                     latitude  : place.geometry.location.lat(),
                     longitude : place.geometry.location.lng()
-                });                            
+                });
                 theView.places.add(placeModel); // Now add it to the collection
-            });                        
+            });
           }
-        });        
+        });
       },
-      addPlaceLi: function (model) {    
+      addPlaceLi: function (model) {
         //The parameter passed is a reference to the model that was added
         $("#places").append(template(model));
         var icon = new google.maps.MarkerImage(model.get('icon'), null, null, null, new google.maps.Size(20, 20));
-        var marker = new google.maps.Marker({ 
-            map      : map, 
-            position : new google.maps.LatLng(model.get('latitude'), model.get('longitude')), 
+        var marker = new google.maps.Marker({
+            map      : map,
+            position : new google.maps.LatLng(model.get('latitude'), model.get('longitude')),
             title      : model.get('name'),
-            icon      : icon        
+            icon      : icon
         });
         map.markers.push(marker);
       }
--- js/ember/app.js
+++ js/ember/app.js
@@ -5,8 +5,8 @@ Places.MapData = Ember.Object.create({
 });
 
 Places.MapView = Ember.View.extend({
-    tagName : 'div',    
-    map : null,               
+    tagName : 'div',
+    map : null,
     didInsertElement : function() { // Create the map
         this._super();
         Places.MapData.set('map', new google.maps.Map($('#map').get(0), {
@@ -15,9 +15,9 @@ Places.MapView = Ember.View.extend({
             zoom: 15
         }));
     }
-});            
+});
 
-Places.placeListController = Ember.ArrayController.create();            
+Places.placeListController = Ember.ArrayController.create();
 
 Places.searchController = Ember.Object.create({
     searchText: 'restaurants', // Set the value of the search text
@@ -30,10 +30,10 @@ Places.searchController = Ember.Object.create({
         };
         var _this = this;
         var service = new google.maps.places.PlacesService(this.get('map'));
-        service.search(request, function callback(results, status) {                        
-            if (status == google.maps.places.PlacesServiceStatus.OK) {                                                                            
+        service.search(request, function callback(results, status) {
+            if (status == google.maps.places.PlacesServiceStatus.OK) {
                 results = results.slice(0, 10); // We want only the first 10 results
-                Places.placeListController.set('content', results);    // Set the data for the controller    
+                Places.placeListController.set('content', results);    // Set the data for the controller
                 if (typeof _this.map.markers !== 'undefined') { // Clear the map markers
                     $(_this.map.markers).each(function (index, marker){
                         marker.setMap(null);
@@ -42,17 +42,17 @@ Places.searchController = Ember.Object.create({
                 _this.map.markers = [];
                 $(results).each(function(index, item){ // Create map markers
                     var icon = new google.maps.MarkerImage(item.icon, null, null, null, new google.maps.Size(20, 20));
-                    var marker = new google.maps.Marker({ 
-                        map      : _this.map, 
-                        position : new google.maps.LatLng(item.geometry.location.lat(), item.geometry.location.lng()), 
+                    var marker = new google.maps.Marker({
+                        map      : _this.map,
+                        position : new google.maps.LatLng(item.geometry.location.lat(), item.geometry.location.lng()),
                         title      : item.name,
-                        icon      : icon        
+                        icon      : icon
                     });
-                    _this.map.markers.push(marker);    
+                    _this.map.markers.push(marker);
                 });
             }
         });
-    }            
+    }
 });
 
 Places.SearchView = Ember.TextField.extend(Ember.TargetActionSupport, { // Trigger the search form when enter is pressed
@@ -60,4 +60,4 @@ Places.SearchView = Ember.TextField.extend(Ember.TargetActionSupport, { // Trigg
     insertNewline: function() {
         this.triggerAction();
     }
-});    
+});
